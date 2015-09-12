87c982aebfe9ee34edcde719a60c0efd2c4ad565
euthelup <euthelup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-12 19:59:02 +0000

    tag style fixes
    git-svn-id: https://plugins.svn.wordpress.org/wp-custom-body-class/trunk@1244176 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings/general.php
+++ settings/general.php
@@ -10,11 +10,24 @@ return array(
 			'type'           => 'switch',
 			'desc' => __( 'Here you can decide if the body class meta box is visible to editors' ),
 		),
+		'enable_autocomplete' => array(
+			'label'          => __( 'Enable Autocomplete', 'custom_body_class_txtd' ),
+			'default'        => true,
+			'type'           => 'switch',
+			'desc' => __( 'Would you like to get auto completed values?' ),
+		),
 		'display_on_post_types' => array(
 			'label'          => __( 'Post Types', 'custom_body_class_txtd' ),
 			'default'        => array('post' => 'on', 'page' => 'on'),
 			'type'           => 'post_types_checkbox',
 			'description' => __( 'Which post types should have fields' ),
-		)
+		),
+
+//		'display_on_taxonomies' => array(
+//			'label'          => __( 'Taxonomies', 'custom_body_class_txtd' ),
+//			'default'        => array( 'category' => 'on' ),
+//			'type'           => 'taxonomies_checkbox',
+//			'description' => __( 'Which taxonomies should have fields' ),
+//		)
 	)
 ); # config
